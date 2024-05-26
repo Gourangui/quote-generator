46424e153170c237e13839816fe5deefc6f176f9
Gouri <gouranguidevi@gmail.com>
2024-05-26 18:32:32 -0300

    função botões + loading spinner

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+/* Show loading */
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+/* Hide loading */
+function removeLoadingSpinner(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 /* Show new quote */
 function newQuote(){
+    showLoadingSpinner();
     /* Pick a random quote from apiQuotes array */
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,11 +38,15 @@ function newQuote(){
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    /* Set quote, hide loader */
     quoteText.textContent = quote.text;
+    removeLoadingSpinner();
 }
 
 /* Get quotes from API */
 async function getQuotes(){
+    showLoadingSpinner();
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
@@ -46,6 +64,10 @@ function tweetQuote(){
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank'); 
 }
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 /* On load */
 getQuotes();
